c15b78c71e4dcce0cb43109591cc2aeb698adafc
Shreyas H S <104050227+Shreyashs98@users.noreply.github.com>
2024-10-26 21:44:28 +0530

    Update index.js

--- ambulance-hotspot-backend/index.js
+++ ambulance-hotspot-backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://ambulance108.vercel.app/', // Update this to your React app's URL
+    origin: 'https://ambulance108.vercel.app', // Update this to your React app's URL
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // If you need to send cookies or authentication headers
 }));
